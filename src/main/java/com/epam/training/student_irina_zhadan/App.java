06b4fcded77167868a99913dbc13a2afb32cd329
Irina Zhadan <irinavzhadan@gmail.com>
2023-07-11 23:09:11 +0300

    initial commit

---
+++ src/main/java/com/epam/training/student_irina_zhadan/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_irina_zhadan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
